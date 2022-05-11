b0e32ebf5174b3512a5a5036016188aa9be9ea23
yongchan <yongchan46@gmail.com>
2022-05-11 23:17:00 +0900

    20200511

--- 기초 공부/1. 자료형/Practice.py
+++ 기초 공부/1. 자료형/Practice.py
@@ -31,11 +31,11 @@ A3.
 yyyymmdd =
 문자열 슬라이싱
 pin = "881120-1068234"
-print(pin[0:6])
+print(pin[:6])
 
 num =
 pin = "881120-1068234"
-print(pin[7:0])
+print(pin[7:])
 
 Q4. pin = "881120-1068234" 성별을 나타내는 숫자 출력
 
@@ -68,3 +68,61 @@ A7.
 a = ['Life', 'is', 'too', 'short']
 result = " ".join(a)
 print(result)
+
+Q8.(1,2,3) 튜플에 값 4를 추가하여 (1,2,3,4)를 만들어 출력해보자
+A8.
+
+a = (1,2,3)
+a = a + (4,)
+print(a) 
+
+Q9. a = dic()
+print(a) # {}
+
+A9.
+오류가 발생하는 코드는? 3번 
+1. a['name'] = 'python'
+2. a[('a',)] = 'python'
+3. a[[1]] = 'python'
+4. [250] = 'python'
+# dictionary의 key 값으로는 변하는 값을 사용할 수 없다. ex) list 
+
+Q10. dictionary a에서ㅏ 'B' 에 해당하는 값을 추출해 보자
+a = {'A':90, 'B':80, 'C':70}
+result =
+print(a) # {'A':90, 'C':70} 출력
+print(result) # 80 출력
+
+A10. 2가지 방법 존재 (a의 일반적인 Value 출력)
+1. a['B'] 출력
+print(a['B'])
+
+2.a.get 활용
+print(a.get('B'))
+
+- 문제풀이 : pop 함수 사용
+result = a.pop(1) : X # dict는 key와 value로 구성되어 있다.
+result = a.pop('B') # Key값만 빼오기
+
+Q11. a 리스트에서 중복 숫자를 제거해보자
+a = [1,1,1,2,2,3,3,3,4,4,5]
+aSet = set(a[:])
+b = list(set(aSet))
+print(b)
+
+A11.
+a = [1,1,1,2,2,3,3,3,4,4,5]
+aSet = set(a[:])
+b = list(set(aSet))
+print(b)
+
+
+Q12. 파이썬 변수의 특징
+a = b = [1,2,3]
+a[1] = 4
+print(b)
+
+A12.
+a = b = [1,2,3] # [1,2,3] 이라는 객체의 주소를 똑같이 a,b에 넣어준다
+a[1] = 4 # 객체의 첫번째 인덱스의 변화 = 주소는 같지만 값은 변했다 [1,4,3]
+print(b) # 주소값이 같으므로 출력 : [1,4,3]
--- 기초 공부/1. 자료형/연습용.py
+++ 기초 공부/1. 자료형/연습용.py
@@ -1,3 +1,4 @@
-l = [1,2,2,3,4]
-newlist = list(set(l))
-print(newlist)
+a = [1,1,1,2,2,3,3,3,4,4,5]
+aSet = set(a[:])
+b = list(set(aSet))
+print(b)
